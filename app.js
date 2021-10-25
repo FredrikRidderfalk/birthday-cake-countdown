64e0c85ca6d7c57e57e1e0cad06b7dc1ef3870bd
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-10-25 13:40:35 +0200

    added song pause/play on clicking cake

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const countdownDisplay = document.querySelector("#countdown");
 const wish = document.querySelector("#wish");
+const cake = document.querySelector(".cake");
+const audio = document.querySelector("#audio");
+let count = 0;
 
-const cakeDate = new Date("2021-10-15T18:00:00");
+const cakeDate = new Date("2021-11-15T18:00:00");
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
@@ -10,11 +13,34 @@ const day = hour * 24;
 const ticker = setInterval(() => {
   const today = new Date();
   const timeRemaining = cakeDate - today;
+  const days = Math.floor(timeRemaining / day);
+  const hours = Math.floor((timeRemaining % day) / hour);
+  const minutes = Math.floor((timeRemaining % hour) / minute);
+  const seconds = Math.floor((timeRemaining % minute) / second);
+
+  if (timeRemaining < -day) {
+    countdownDisplay.innerHTML =
+      "The cake was great! It was delicious and moist🤤";
+    wish.innerHTML = "The cake was not a lie😍";
+    return;
+  }
 
   if (timeRemaining <= 0) {
     countdownDisplay.innerHTML = "It's time for cake!!!";
     wish.innerHTML = "The cake is not a lie😍";
     clearInterval(ticker);
     return;
+  } else {
+    countdownDisplay.innerHTML = `${days} : ${hours} : ${minutes} : ${seconds}`;
   }
 }, second);
+
+function playPauseSong() {
+  if (count == 0) {
+    count = 1;
+    audio.play();
+  } else {
+    count = 0;
+    audio.pause();
+  }
+}
